2f37d48771d138975d8ea58c5b3269c8fe41ed91
pickhardt@google.com <pickhardt@google.com@379c9969-22ad-7527-c008-72bba83cda63>
2011-02-10 18:50:13 +0000

    Improved GAS, and removed GAM from GASI.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-# copied from http://stackoverflow.com/questions/4294060/help-building-a-mac-application-from-python-using-py2app
 # usage: python setup.py py2exe
 #    or  python setup.py py2app
 
@@ -10,11 +9,7 @@ MAIN_SCRIPT  = "gasi.py"
 
 if sys.platform in ("win32", "win64"): # does win64 exist?
     import py2exe
-    setup( windows=[{ "script":MAIN_SCRIPT
-                   }],
-         )
+    setup(windows=[{ "script":MAIN_SCRIPT}])
 elif sys.platform == "darwin":
     import py2app
-    setup( app=[MAIN_SCRIPT], # doesn't include the icon yet
-           setup_requires=["py2app"],
-         )
+    setup(app=[MAIN_SCRIPT], setup_requires=["py2app"]) # doesn't include the icon yet
